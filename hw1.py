7e7ad151ffadebec76799afb2798f0d5685293d8
Karina Szubert <52202473+Karina-00@users.noreply.github.com>
2020-03-18 21:40:49 +0100

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -5,69 +5,35 @@ import pandas as pd
 CONFIRMED_CASES_URL = f"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data" \
                       f"/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv "
 
-"""
-When downloading data it's better to do it in a global scope instead of a function.
-This speeds up the tests significantly
-"""
+
 confirmed_cases = pd.read_csv(CONFIRMED_CASES_URL, error_bad_lines=False)
 
 
 def poland_cases_by_date(day: int, month: int, year: int = 2020) -> int:
-    """
-    Returns confirmed infection cases for country 'Poland' given a date.
-
-    Ex.
-    >>> poland_cases_by_date(7, 3, 2020)
-    5
-    >>> poland_cases_by_date(11, 3)
-    31
-
-    :param year: 4 digit integer representation of the year to get the cases for, defaults to 2020
-    :param day: Day of month to get the cases for as an integer indexed from 1
-    :param month: Month to get the cases for as an integer indexed from 1
-    :return: Number of cases on a given date as an integer
-    """
-    
-    # Your code goes here (remove pass)
-    pass
+    year = str(year)[2:]
+    return confirmed_cases.loc[confirmed_cases["Country/Region"] == "Poland"][f"{month}/{day}/{year}"].values[0]
 
 
 def top5_countries_by_date(day: int, month: int, year: int = 2020) -> List[str]:
-    """
-    Returns the top 5 infected countries given a date (confirmed cases).
+    year = str(year)[2:]
+    date = f'{month}/{day}/{year}'
 
-    Ex.
-    >>> top5_countries_by_date(27, 2, 2020)
-    ['China', 'Korea, South', 'Cruise Ship', 'Italy', 'Iran']
-    >>> top5_countries_by_date(12, 3)
-    ['China', 'Italy', 'Iran', 'Korea, South', 'France']
+    x = confirmed_cases[["Country/Region", date]].sort_values(by=date)
+    new_df = x.groupby("Country/Region").sum().sort_values(by=date).tail(5).sort_values(by=date, ascending=False)
 
-    :param day: 4 digit integer representation of the year to get the countries for, defaults to 2020
-    :param month: Day of month to get the countries for as an integer indexed from 1
-    :param year: Month to get the countries for as an integer indexed from 1
-    :return: A list of strings with the names of the coutires
-    """
+    result = list(new_df.T)
+    return result
+  
 
-    # Your code goes here (remove pass)
-    pass
+  def no_new_cases_count(day: int, month: int, year: int = 2020) -> int:
+    year = str(year)[2:]
+    date = f'{month}/{day}/{year}'
 
-# Function name is wrong, read the pydoc
-def no_new_cases_count(day: int, month: int, year: int = 2020) -> int:
-    """
-    Returns the number of countries/regions where the infection count in a given day
-    was NOT the same as the previous day.
+    columns = list(confirmed_cases)
+    idx = columns.index(date)
+    day_before = columns[idx-1]
 
-    Ex.
-    >>> no_new_cases_count(11, 2, 2020)
-    35
-    >>> no_new_cases_count(3, 3)
-    57
-
-    :param day: 4 digit integer representation of the year to get the cases for, defaults to 2020
-    :param month: Day of month to get the countries for as an integer indexed from 1
-    :param year: Month to get the countries for as an integer indexed from 1
-    :return: Number of countries/regions where the count has not changed in a day
-    """
-    
-    # Your code goes here (remove pass)
-    pass
+    data_set = confirmed_cases[["Country/Region", day_before, date]]
+    confirmed_cases['new_today'] = confirmed_cases[date] - confirmed_cases[day_before]
+    new_cases = confirmed_cases.loc[confirmed_cases['new_today'] != 0].count()[-1]
+    return new_cases
